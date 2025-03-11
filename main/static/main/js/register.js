f4384c21007a1edb004c1f02ac71545b4c3b0131
wvvtop <sav.sanya00@mail.ru>
2025-03-11 13:06:28 +0700

    Добавил билеты пдд

---
+++ main/static/main/js/register.js
@@ -0,0 +1,8 @@
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    if (input.type === 'password') {
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
+}
