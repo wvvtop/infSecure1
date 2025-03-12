013ad73543456f455c3fec0b4971262e83807509
wvvtop <sav.sanya00@mail.ru>
2025-03-12 15:12:45 +0700

    Добавил билеты пдд

--- main/static/main/js/register.js
+++ main/static/main/js/register.js
@@ -1,8 +1,12 @@
 function togglePassword(inputId) {
     const input = document.getElementById(inputId);
+    const icon = document.querySelector(`#${inputId} ~ .password-toggle-icon`); // Adjusted selector for sibling relationship
+
     if (input.type === 'password') {
         input.type = 'text';
+        icon.textContent = '😐'; // Change to "hide" icon
     } else {
         input.type = 'password';
+        icon.textContent = '😑'; // Change back to "show" icon
     }
 }
